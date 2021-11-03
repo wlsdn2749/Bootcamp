79737311d4a9895a8252e7e13467ac5dfc55a8ce
MoonSheep08 <gus5tmd24@naver.com>
2021-11-04 00:47:06 +0900

    가게 위도 경도 추가 테이블 작성

--- bootrc/forms.py
+++ bootrc/forms.py
@@ -7,10 +7,12 @@ class MenuForm(forms.ModelForm):
         model = Menu  # 사용할 모델
         fields = ['menu_catenum', 'menu_name2']  # MenuForm 에서 사용할 Menu모델의 2가지 속성
 
+
 class RestForm(forms.ModelForm):
     class Meta:
         model = Rest  # 사용할 모델
-        fields = ['rest_name', 'rest_star']  # RestForm에서 사용할 rest모델의 1가지 속성
+        fields = ['rest_name', 'rest_star', 'rest_location_lat', 'rest_location_lon']
+        # RestForm에서 사용할 rest모델의 1가지 속성
 
 
 class RestMenuForm(forms.ModelForm):
--- bootrc/views.py
+++ bootrc/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Menu, Rest, RestMenu
 from .forms import MenuForm, RestForm, RestMenuForm
+import math
 
 def main(request):  # 회원, 비회원을 구분하는 페이지
     return render(request, 'bootrc/main_page.html')
@@ -43,9 +44,10 @@ def menu_create(request):
         form = MenuForm()
     return render(request, 'bootrc/menu_form.html', {'form': form})
 
+
 def rest_create(request):
     '''
-    메뉴 등록
+    가게 등록
     '''
     if request.method == 'POST':
         form = RestForm(request.POST)
@@ -56,6 +58,7 @@ def rest_create(request):
     else:
         form = RestForm()
     return render(request, 'bootrc/rest_form.html', {'form': form})
+
 
 def restmenu_create(request, rest_rest_num):
     '''
@@ -74,11 +77,18 @@ def restmenu_create(request, rest_rest_num):
     rest_menu = {'rest': rest, 'form': form}
     return render(request, 'bootrc/restmenu_form.html', rest_menu)
 
+# 테스트 해봐야 함
+def distance_from_college(request, rest_rest_num):
+    """
+    학교에서 가게 위치 거리 계산
+    전주대 후문 위도 경도 : 35.817094, 127.090152 lat
+    전주대 정문 위도 경도 : 35.813841, 127.094034 lon
+    """
+    rest_location = rest_rest_num
+    if request.method == 'POST':
+        rest_location_lat = rest_location.rest_location_lat
+        rest_location_lon = rest_location.rest_location_lon
+        distance = math.sqrt((35.817094-rest_location_lat)+(35.813841-rest_location_lon))
+#    return render(request, 'bootrc/rest_list.html', {'distance': distance})
+    return render(request, 'bootrc/rest_list.html', {'distance': 4})
 
-# def recommend_algorithm_delivery(request, rest_rest_num):
-    """
-    음식추천 알고리즘 배달
-    전주대 후문 위도 경도 : 35.817094, 127.090152
-    전주대 정문 위도 경도 : 35.813841, 127.094034
-    """
-#   rest = get_object_or_404(Rest, pk=rest_rest_num)
