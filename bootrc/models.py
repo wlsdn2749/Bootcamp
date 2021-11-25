5191a85c92ba1eb25a50e3caf6317eee8de11c86
Jinwoo Jung <wlsdn2749@naver.com>
2021-11-25 14:51:57 +0900

    메인 링크 연결 모델 오타 수정 url 변ㄱㅇ 등등

--- bootrc/models.py
+++ bootrc/models.py
@@ -63,7 +63,7 @@ class Rest(models.Model):
         count = 0
         review_rating_avg = 0
         app_review_rating_avg = 0
-        for j in review_ratigs:
+        for j in review_ratings:
             count += j.rating
         if review_ratings:
             review_rating_avg = count / len(review_ratings)
